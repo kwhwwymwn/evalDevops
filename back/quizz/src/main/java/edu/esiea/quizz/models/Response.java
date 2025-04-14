f568db75667800a581e9a496703df85b1783edd9
matheis <m.giammarinaro@groupe-aen.info>
2025-04-14 16:27:13 +0200

    bug fixes

--- back/quizz/src/main/java/edu/esiea/quizz/models/Response.java
+++ back/quizz/src/main/java/edu/esiea/quizz/models/Response.java
@@ -1,10 +1,12 @@
 package edu.esiea.quizz.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Response {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
